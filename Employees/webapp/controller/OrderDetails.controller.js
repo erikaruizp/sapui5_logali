1d3698693cda8444e340a748ccaaca1a8ec3b6f2
erikaruizp <erikaruizp@gmail.com>
2021-05-14 02:10:27 +0000

    13_Navegacion_v3

--- Employees/webapp/controller/OrderDetails.controller.js
+++ Employees/webapp/controller/OrderDetails.controller.js
@@ -1,12 +1,33 @@
 sap.ui.define([
         "sap/ui/core/mvc/Controller",
+        "sap/ui/core/routing/History"
 	],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
+     * @param {typeof sap.ui.routing.History} History
      */
-    function (Controller) {
-      
+    function (Controller,History) {
+        function _onObjectMatch(oEvent) {
+            this.getView().bindElement({
+                path: '/Orders(' + oEvent.getParameter("arguments").OrderID + ')',
+                model: 'odataNorthwind'
+            });
+        }
         return Controller.extend("logaligroup.Employees.controller.OrderDetails", {
-            onInit: function () {  }
+            onInit: function () { 
+                var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                oRouter.getRoute("RouteOrderDetails").attachPatternMatched(_onObjectMatch, this);
+            },
+            onBack: function (oEvent) {
+                var oHistory = History.getInstance();
+                var sPrevHash = oHistory.getPreviousHash();
+
+                if (sPrevHash !== undefined) {
+                    window.history.go(-1);
+                } else {
+                    var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
+                    oRouter.navTo("RouteMain", true);
+                }
+            }
         });
 }); 
