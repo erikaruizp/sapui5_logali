2d507b0c327d536091e602a5baf23829b90d3452
erikaruizp <erikaruizp@gmail.com>
2021-05-21 17:50:13 +0000

    18_Despliegue_en_Cloud

--- Employees/webapp/controller/OrderDetails.controller.js
+++ Employees/webapp/controller/OrderDetails.controller.js
@@ -182,7 +182,7 @@ sap.ui.define([
         downloadFile: function (oEvent) {
             let sPath = oEvent.getSource().getBindingContext("incidenceModel").getPath();
 
-            window.open("/sap/opu/odata/sap/YSAPUI5_SRV_01" + sPath + "/$value");
+            window.open("sap/opu/odata/sap/YSAPUI5_SRV_01" + sPath + "/$value");
         }
         
         });
